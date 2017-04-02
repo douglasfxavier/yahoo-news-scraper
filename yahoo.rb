7cd20bc3c0a0c1a7108532131bba9d4e2cd71d68
douglasfxavier <douglasf.xavier@gmail.com>
2017-04-01 23:10:08 -0300

    Alterando os paths, simplificando

--- yahoo.rb
+++ yahoo.rb
@@ -8,21 +8,24 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = 1
 
 	@arquivo_saida = File.open("News.csv", 'w')
+	@arquivo_saida.puts "index|headline|lead"
 	
 
 def get_news (url)
 
 	doc = Nokogiri::HTML(open(url))
-	@arquivo_saida.puts "index|headline|lead"
+
+	test_result_page = File.open("test_page.html", "w")
+	test_result_page.write(doc.to_s)
 
 	index = 0
 
-	doc.xpath('.//ul[@class = "Bleed Mt-neg-10 Mb-0 ResetChildren Wow-bw Divided-h"]/li').each_with_index do |li,i|
-		
+	doc.xpath('.//ul[@id="Stream"]/li').each do |li|
+
 		#Iterando sobre as notícias
-		headline = li.xpath('.//a[@class = "js-stream-content-link Mend-4"]/text()')
-		#next if headline.empty?
-		lead  = li.xpath('.//p[@class = "Mb-2"]/text()')
+		headline = li.xpath('.//h3/a//text()')
+		next if headline.empty?
+		lead  = li.xpath('.//div/p/text()')
 		index += 1
 		single_news = News.new(index,headline,lead)
 
